60509ab644b5744d3792429fd76f86b38abd06bc
jos26262626-a11y <jos26262626@gmail.com>
2025-09-30 17:49:26 -0600

    Actualizado archivo de configuración

--- app/core/config.py
+++ app/core/config.py
@@ -1,3 +1,4 @@
+from pydantic import BaseModel
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
@@ -7,12 +8,13 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
     GOOGLE_CLIENT_ID: str = ""
     ALLOWED_EMAIL_DOMAIN: str | None = None
-  
-    CORS_ORIGINS: str = "*"   
-    ROOT_PATH: str = ""       
-    DOCS_URL: str = "/docs"   
+    CORS_ORIGINS: str = "*"
+    ROOT_PATH: str = ""
+    DOCS_URL: str = "/docs"
 
     class Config:
         env_file = ".env"
+        env_file_encoding = "utf-8"
+        case_sensitive = True
 
 settings = Settings()
